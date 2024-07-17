a80ad9385ebf3393fc77418e1e71eb4e71aa0fa4
skylexwilly <merngowilson2@gmail.com>
2024-07-17 10:39:15 +0300

    javascript-functions-continued

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate') {
+  return `This Saturday, I want to ${activity}!`;
+}
+
+// Export the function if using modules
+//function mondayWork(activity = 'go to the office') {
+//  return This Monday, I will ${activity}.;
+//}
+
+const mondayWork = function(activity = 'go to the office') {
+  return `This Monday, I will ${activity}.`;
+};
+
+function wrapAdjective(delimiter = '*') {
+  return function(adjective) {
+    return `You are ${delimiter}${adjective}${delimiter}!`;
+  };
+}
